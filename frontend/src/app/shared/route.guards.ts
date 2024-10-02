66cf9a60a74d9f438ab8923ca5805aa7d32454c8
Jeffry Gonzalez <jeff@hypertheory.com>
2024-10-02 09:15:09 -0400

    weds am

--- frontend/src/app/shared/route.guards.ts
+++ frontend/src/app/shared/route.guards.ts
@@ -1,23 +1,20 @@
 import { inject } from '@angular/core';
 import { toObservable } from '@angular/core/rxjs-interop';
-import { UserStore } from './user.store';
 import {
   ActivatedRouteSnapshot,
-  CanActivateChildFn,
   CanActivateFn,
   Router,
   RouterStateSnapshot,
 } from '@angular/router';
-import { first, map, tap } from 'rxjs';
+import { UserStore } from './user.store';
 
 // return a boolean, signal(boolean), observable(boolean), route tree
 export function userLoggedInGuard(): CanActivateFn {
-  let url = '';
   return (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
     const router = inject(Router);
     const userStore = inject(UserStore);
     const obs = toObservable(userStore.userLoggedIn);
-    url = state.url;
+
     if (userStore.userLoggedIn()) {
       return true;
     } else {
